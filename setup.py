277316c27a41c378f02d8e477b0212b815022a76
Kevin <kmanc@comcast.net>
2019-02-25 16:42:10 -0800

    version for next release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="cryptopals",
-    version="0.0.1",
+    version="0.0.2",
     author="Kevin Conley",
     author_email="kmanc@comcast.net",
     description="Matasano cryptopals challenges",
