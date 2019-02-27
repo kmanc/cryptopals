731a58e18e0012b0b629e243e1f9a2f16671f6b7
Kevin <kmanc@comcast.net>
2019-02-27 12:26:19 -0800

    validating that challenge 4 works

--- tests/test_set_1.py
+++ tests/test_set_1.py
@@ -36,5 +36,16 @@ class TestChallenges:
 
         assert attack_output.upper() == answer_file.upper()
 
+    @staticmethod
+    def test_challenge_3():
+        """Break single byte XOR"""
+
+        challenge_file = file.to_lines("tests/challenge_inputs/set_1/challenge_4.txt")
+        answer_file = file.to_string("tests/challenge_answers/set_1/challenge_4.txt")
+
+        attack_output = set_1.find_and_break_repeat_hex_char_xor(challenge_file)
+
+        assert attack_output.upper() == answer_file.upper()
+
 
 
