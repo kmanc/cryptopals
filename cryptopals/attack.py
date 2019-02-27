655daefb2b2968d287be4c59330bed6f3d872e0e
Kevin <kmanc@comcast.net>
2019-02-27 09:58:06 -0800

    challenge 2 complete. also attacks should exactly mirror input/output from challenge

--- cryptopals/attack.py
+++ cryptopals/attack.py
@@ -9,6 +9,18 @@ def hex_to_base64(input_hex):
     """Converts hex to base64"""
 
     input_bytes = convert.hex_to_bytes(input_hex)
-    output = convert.bytes_to_base64(input_bytes)
+    bytes_result = convert.bytes_to_base64(input_bytes)
+    output = convert.bytes_to_ascii(bytes_result)
+
+    return output
+
+
+def fixed_size_hex_xor(input_string_1, input_string_2):
+    """Takes in 2 hex strings and XORs them"""
+
+    string_1_as_bytes = convert.hex_to_bytes(input_string_1)
+    string_2_as_bytes = convert.hex_to_bytes(input_string_2)
+    xor_result = xor.fixed_size(string_1_as_bytes, string_2_as_bytes)
+    output = convert.bytes_to_hex(xor_result)
 
     return output
