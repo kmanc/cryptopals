ed4faa3bdcf63410d7f9b242e800af6211bf6482
Kevin <kmanc@comcast.net>
2019-02-27 10:53:21 -0800

    test challenge 3

--- tests/test_set_1.py
+++ tests/test_set_1.py
@@ -25,3 +25,17 @@ class TestChallenges:
         attack_output = attack.fixed_size_hex_xor(challenge_file[0], challenge_file[1])
 
         assert attack_output == answer_file
+
+    @staticmethod
+    def test_challenge_3():
+        """Break single byte XOR"""
+
+        challenge_file = file.to_string("tests/challenge_inputs/set_1/challenge_3.txt")
+        answer_file = file.to_string("tests/challenge_answers/set_1/challenge_3.txt")
+
+        attack_output = attack.break_repeat_hex_char_xor(challenge_file)
+
+        assert attack_output.upper() == answer_file.upper()
+
+
+
