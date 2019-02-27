0e1b1367cb9664a39b62a8b793f11e1386a4d32a
Kevin <kmanc@comcast.net>
2019-02-27 10:00:12 -0800

    testing for challenges 1 and 2

--- tests/test_set_1.py
+++ tests/test_set_1.py
@@ -6,12 +6,22 @@ class TestChallenges:
 
     @staticmethod
     def test_challenge_1():
-        """Hex to base64
-        https://cryptopals.com/sets/1/challenges/1"""
+        """Hex to base64"""
 
-        challenge_file = file.to_string("tests/challenge_inputs/set_1_challenge_1.txt")
-        answer_file = file.to_bytes("tests/challenge_answers/set_1_challenge_1.txt")
+        challenge_file = file.to_string("tests/challenge_inputs/set_1/challenge_1.txt")
+        answer_file = file.to_string("tests/challenge_answers/set_1/challenge_1.txt")
 
         attack_output = attack.hex_to_base64(challenge_file)
+
+        assert attack_output == answer_file
+
+    @staticmethod
+    def test_challenge_2():
+        """Fixed size hex string XOR"""
+
+        challenge_file = file.to_lines("tests/challenge_inputs/set_1/challenge_2.txt")
+        answer_file = file.to_string("tests/challenge_answers/set_1/challenge_2.txt")
+
+        attack_output = attack.fixed_size_hex_xor(challenge_file[0], challenge_file[1])
 
         assert attack_output == answer_file
