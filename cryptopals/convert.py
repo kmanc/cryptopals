b0163e72c883a99d8049c1a45f69a28932a3192a
Kevin <kmanc@comcast.net>
2019-02-28 09:54:35 -0800

    base64 to bytes conversion doc updated

--- cryptopals/convert.py
+++ cryptopals/convert.py
@@ -12,7 +12,7 @@ def ascii_to_hex(input_1):
 
 
 def base64_to_bytes(input_1):
-    """Takes in a byte string. Outputs a base64 encoded byte string"""
+    """Takes in a base64 encoded byte string. Outputs a byte string"""
     return base64.b64decode(input_1)
 
 
