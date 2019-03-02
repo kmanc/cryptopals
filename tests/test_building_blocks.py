88c5aafc12ccf4c4f3b3fe933f4ab382e971a104
Kevin <kmanc@comcast.net>
2019-03-01 20:53:50 -0800

    tested unpadder

--- tests/test_building_blocks.py
+++ tests/test_building_blocks.py
@@ -218,4 +218,16 @@ class TestPad:
         assert my_output_1 == desired_output_1
         assert my_output_2 == desired_output_2
 
+    @staticmethod
+    def test_undo_pkcs_7_pad():
+        """Tests undoing PKCS#7 padding"""
 
+        test_input_1 = b"YELLOW SUBMARINE\x04\x04\x04\x04"
+        desired_output_1 = b'YELLOW SUBMARINE'
+        my_output_1 = pad.undo_pkcs_7(test_input_1)
+        test_input_2 = b"YELLOW SUBMARINE\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10"
+        desired_output_2 = b'YELLOW SUBMARINE'
+        my_output_2 = pad.undo_pkcs_7(test_input_2)
+
+        assert my_output_1 == desired_output_1
+        assert my_output_2 == desired_output_2
