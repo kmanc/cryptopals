cede58bb119098c17ee91da9f9bce46aafca7b20
Kevin <kmanc@comcast.net>
2019-03-01 20:52:56 -0800

    added pkcs unpadder

--- cryptopals/pad.py
+++ cryptopals/pad.py
@@ -4,3 +4,17 @@ def pkcs_7(byte_string, length):
     byte_string += bytes([size_needed]) * size_needed
 
     return byte_string
+
+
+def undo_pkcs_7(byte_string):
+    """Take in a byte string that has been PKCS padded. Output the unpadded byte string or a value error if
+    padding is not valid"""
+    padded = True
+    last_byte = byte_string[-1]
+    for char in byte_string[-last_byte:]:
+        if char != last_byte:
+            padded = False
+
+    if padded is True:
+        return byte_string[:-last_byte]
+    raise ValueError('Invalid padding')
