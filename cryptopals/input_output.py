77dcf09e5ebe1a76e1f4901aaeb9568d83d3153d
Kevin <kmanc@comcast.net>
2020-03-29 14:58:25 -0700

    add and test string repeater / shortener

--- cryptopals/input_output.py
+++ cryptopals/input_output.py
@@ -1,6 +1,18 @@
+import math
+
+
 def hex_to_bytes(input_hex):
     return bytes.fromhex(input_hex)
 
 
 def bytes_to_hex(input_bytes):
     return input_bytes.hex()
+
+
+def resize_bytes(input_bytes, length):
+    multiplier = math.ceil(length / len(input_bytes))
+    raw_output = input_bytes * multiplier
+
+    return raw_output[:length]
+
+
