ce0a931530fe9fe2c61a7e2550924989b51d85c7
Kevin <kmanc@comcast.net>
2020-04-01 10:54:29 -0700

    deal with file renames

--- cryptopals/_challenge_1.py
+++ cryptopals/_challenge_1.py
@@ -4,4 +4,4 @@ import base64
 
 def complete_challenge(challenge_input):
     bytes_input = input_output.hex_to_bytes(challenge_input)
-    return _base64.base64_encode(bytes_input)
+    return base64.base64_encode(bytes_input)
--- cryptopals/_challenge_2.py
+++ cryptopals/_challenge_2.py
@@ -5,6 +5,6 @@ import xor
 def complete_challenge(challenge_input_1, challenge_input_2):
     bytes_input_1 = input_output.hex_to_bytes(challenge_input_1)
     bytes_input_2 = input_output.hex_to_bytes(challenge_input_2)
-    xored_byte_list = _xor.fixed_length(bytes_input_1, bytes_input_2)
+    xored_byte_list = xor.fixed_length(bytes_input_1, bytes_input_2)
     xored_bytes = bytes(xored_byte_list)
     return input_output.bytes_to_hex(xored_bytes)
