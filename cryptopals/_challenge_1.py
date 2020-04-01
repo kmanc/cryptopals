e0e4f7bcdbb617abe0770188b776f20a53e5ae0d
Kevin <kmanc@comcast.net>
2020-04-01 11:38:44 -0700

    challenges 1-3 complete

--- cryptopals/_challenge_1.py
+++ cryptopals/_challenge_1.py
@@ -1,7 +1,12 @@
 import input_output
 import base64
 
+challenge_input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+
 
 def complete_challenge(challenge_input):
     bytes_input = input_output.hex_to_bytes(challenge_input)
     return base64.base64_encode(bytes_input)
+
+
+print(complete_challenge(challenge_input))
--- cryptopals/_challenge_2.py
+++ cryptopals/_challenge_2.py
@@ -2,9 +2,16 @@ import input_output
 import xor
 
 
+challenge_input_1 = "1c0111001f010100061a024b53535009181c"
+challenge_input_2 = "686974207468652062756c6c277320657965"
+
+
 def complete_challenge(challenge_input_1, challenge_input_2):
     bytes_input_1 = input_output.hex_to_bytes(challenge_input_1)
     bytes_input_2 = input_output.hex_to_bytes(challenge_input_2)
     xored_byte_list = xor.fixed_length(bytes_input_1, bytes_input_2)
     xored_bytes = bytes(xored_byte_list)
     return input_output.bytes_to_hex(xored_bytes)
+
+
+print(complete_challenge(challenge_input_1, challenge_input_2))
