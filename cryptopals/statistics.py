1aeb2191ce0ad5d94dfb70ee2a084578f95438f8
Kevin <kmanc@comcast.net>
2020-04-01 11:38:03 -0700

    reorder numbers for readability

--- cryptopals/statistics.py
+++ cryptopals/statistics.py
@@ -1,8 +1,33 @@
 def english_score(input_bytes):
-    # The following set contains the decimal number associated with the most common english characters
-    # ('ETAOIN SHRDLU') in both upper and lower case, also including the space
-    point_worthy = {69, 101, 84, 116, 65, 97, 79, 111, 73, 105, 78, 110,
-                    32, 83, 115, 72, 104, 82, 114, 68, 100, 76, 108, 85, 117}
+    # The following set contains the decimal number that corresponds to the ASCII for the most common english
+    # characters ('ETAOIN SHRDLU') in both upper and lower case, also including the space
+    point_worthy = {
+        32,
+        65,
+        68,
+        69,
+        72,
+        73,
+        76,
+        78,
+        79,
+        82,
+        83,
+        84,
+        85,
+        97,
+        101,
+        104,
+        105,
+        100,
+        108,
+        110,
+        111,
+        114,
+        115,
+        116,
+        117
+    }
 
     chars_in_point_worthy = list(filter(point_worthy.__contains__, input_bytes))
 
