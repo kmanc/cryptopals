9614586510cff9ed136cacdb13504333fb552154
Kevin <kmanc@comcast.net>
2020-05-22 18:50:30 -0700

    cleared up some things messing with my dist

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.MD", "r") as f:
 
 setup(
     name="cryptopals",
-    version="0.0.3",
+    version="0.0.5",
     author="Kevin Conley",
     author_email="kmancxc@gmail.com",
     description="Provides building blocks that can be used to complete Matasano Cryptopals Challenges",
     license="MIT",
     keywords="Python, Crypto, Cryptopals",
     url="https://github.com/kmanc/cryptopals",
-    packages=["cryptopals"],
+    packages=["cryptopals", "cryptopals.attacks", "cryptopals.building_blocks"],
     long_description=long_description,
     python_requires=">=3.6",
     classifiers=[
