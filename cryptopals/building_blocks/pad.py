b36f9d2bcf41e39efae19b08e2673cb867eb6587
Kevin <kmanc@comcast.net>
2020-05-29 21:52:01 -0700

    forgot a docstring

--- cryptopals/building_blocks/pad.py
+++ cryptopals/building_blocks/pad.py
@@ -1,4 +1,5 @@
 def pkcs_7_pad(input_bytes: bytes, length: int):
+    """Takes in a byte string and an integer. Outputs a byte string padded to a block size of integer following PKCS#7 standards"""
     size_needed = length - (len(input_bytes) % length)
     padded_bytes = input_bytes + (bytes([size_needed]) * size_needed)
 
